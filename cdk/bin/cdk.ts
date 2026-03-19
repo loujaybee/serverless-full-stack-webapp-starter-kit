f00fa213e218a555ed75d64a98d4bf46d96dd7ca
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2026-03-19 13:36:18 +0900

    chore(cdk): add Application tag to all stacks (#97)
    ## Summary
    - Add `Application: ServerlessWebappStarterKit` tag to the entire CDK
    app
    - Uses `cdk.Tags.of(app).add()` to automatically propagate the tag to
    all resources across both stacks

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -52,6 +52,8 @@ new MainStack(app, 'ServerlessWebappStarterKitStack', {
   signPayloadHandler: virginia.signPayloadHandler,
 });
 
+cdk.Tags.of(app).add('Application', 'ServerlessFullStackWebappStarterKit');
+
 // import { Aspects } from 'aws-cdk-lib';
 // import { AwsSolutionsChecks } from 'cdk-nag';
 // Aspects.of(app).add(new AwsSolutionsChecks());
